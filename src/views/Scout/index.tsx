02f837fbc319b50b34b8219974bef922ca428bae
RohanTyagi7 <rohan.x.tyagi@gmail.com>
2023-08-07 09:52:17 -0400

    refinements

--- src/views/Scout/index.tsx
+++ src/views/Scout/index.tsx
@@ -3,10 +3,12 @@ import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import ScoutForm from './ScoutForm'
+import { Link } from "react-router-dom";
 
 function ReportsContent() {
   return (
     <React.Fragment>
+      <Link to="/reports" ><div style={{height: "60px", width: "60px", borderRadius: "100%", backgroundColor: "green", position: "fixed", bottom: "10px", right: "10px", cursor: "pointer"}}><img src="https://i.imgur.com/ekphRxF.png" width="90%"  /></div></Link>
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         {/* Recent Orders */}
         <Grid item xs={12}>
